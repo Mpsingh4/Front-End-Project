5d85d1e4ae88d4243afccaef523dae38973d8c04
Manpreet Saini <mp_manpreet@hotmail.com>
2024-11-20 15:29:13 -0500

    downloaded assets

--- responsive-website/src/components/navbar/navbar.jsx
+++ responsive-website/src/components/navbar/navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons';
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='whatWS__navbar'>
+      <div className='whatWS__navbar-links'>
+        <div className='whatWS__navbar-links-logo'>
+              
+        </div>
+      </div>
       Navbar
     </div>
   )
 }
 
 export default Navbar
+//<img src={logo} alt='logo' />
