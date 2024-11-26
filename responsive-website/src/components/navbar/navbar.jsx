a47e9a2aeab7949fd8f5e18bfcfb5eac73d9447a
Mpsingh4 <mp_manpreet@hotmail.com>
2024-11-25 22:08:46 -0500

    issues fixed

--- responsive-website/src/components/navbar/navbar.jsx
+++ responsive-website/src/components/navbar/navbar.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
-import { RiMenu3Line, RiCloseLine } from 'react-icons';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
-//menu for mobile here
+const Menu = () => (
+  <>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#whatWS'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+        </>
+)
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -41,4 +49,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-//<img src={logo} alt='logo' />
