9b7e22760806ea229f23874af82dadeed5898dd1
Manpreet Saini <mp_manpreet@hotmail.com>
2025-02-03 14:27:55 -0500

    initial possibilities tab with minor css

--- responsive-website/src/containers/possibility/pos.jsx
+++ responsive-website/src/containers/possibility/pos.jsx
@@ -10,7 +10,7 @@ const Pos = () => {
       </div>
       <div className='whatWS__pos-content'>
         <h4>Request Early Access to Get Started</h4>
-        <h1 className='gradient-text'> The Possibilities are Endless.</h1>
+        <h1 className='gradient__text'> The Possibilities are Endless.</h1>
         <p>The possibilities are vast, with potential applications across various fields such as education, healthcare, customer service, and creative writing. It could be used to automate routine tasks, assist in research, or provide personalized recommendations, enhancing productivity. As AI continues to evolve, ChatGPT may open new avenues for human-computer collaboration.</p>
       </div>
     </div>
