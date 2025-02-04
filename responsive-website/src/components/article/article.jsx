aced70f7ee194de377e1ff8a6694d7da2a392815
Manpreet Saini <mp_manpreet@hotmail.com>
2025-02-04 13:26:00 -0500

    pt2 of previous commit, filed were not correctly staged for push

--- responsive-website/src/components/article/article.jsx
+++ responsive-website/src/components/article/article.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import './article.css';
 
-const article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className='whatWS__blog-container_article'>
       <div className='whatWS__blog-container_article-image'>
-        <img src={imgUrl} alt='blog image' />
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='whatWS__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
       </div>
     </div>
   )
 }
 
-export default article
+export default Article;
