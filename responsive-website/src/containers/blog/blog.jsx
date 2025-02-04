1b9db446ee3759b90be6dda6745fb8f7280f3b22
Manpreet Saini <mp_manpreet@hotmail.com>
2025-02-04 12:36:42 -0500

    initiate articles

--- responsive-website/src/containers/blog/blog.jsx
+++ responsive-website/src/containers/blog/blog.jsx
@@ -3,8 +3,22 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className='whatWS__blog section__padding' id='blog'>
+      <div className='whatWS__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it!</h1>
+      </div>
+      <div className='whatWS__blog-container'>
+        <div className='whatWS__blog-container_groupA'>
+          Article 
+        </div>
+        <div className='whatWS__blog-container_groupA'>
+          article 
+          article 
+          article
+          article 
+        </div>
+        
+      </div>
     </div>
   )
 }
